f3b6f63a6477f7b2f9c03230d919caf5338f705c
Henry <henryk33658@gmail.com>
2025-07-09 23:30:52 -0300

    Add example using let variables in JavaScript
    Introduces a new file demonstrating variable declarations with let, including string, number, and boolean types, and outputs a formatted message to the console.

---
+++ Aula 2/Variáveis/let.js
@@ -0,0 +1,6 @@
+// variáveis let
+let name = "João";
+let age = 30;
+let address = "Rua A, 123";
+let isStudent = true;
+console.log("Olá, meu nome é " + name + ", tenho " + age + " anos, moro em " + address + " e é " + isStudent + " que sou estudante.");
