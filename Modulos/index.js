ae5c27db6c925b68ee33025f766c90e32a01e91b
henrykhsa <henryk33658@gmail.com>
2025-07-26 23:35:59 -0300

    Add lodash shuffle example to index.js
    Imported lodash and demonstrated its shuffle function by shuffling an array of numbers and logging both the original and shuffled arrays.

--- Modulos/index.js
+++ Modulos/index.js
@@ -1,7 +1,13 @@
 import { saudacao } from './utils.js';
 import { somar } from './calculos.js';
 import multiplicar from './calculos.js';
+import _ from 'lodash';
 
 console.log(saudacao("Henry"));
 console.log(somar(5, 3));
 console.log(multiplicar(4, 10));
+
+const numeros = [1, 2, 3, 4, 5];
+const numerosEmbaralhados = _.shuffle(numeros);
+console.log("numeros: ", numeros);
+console.log("numerosEmbaralhados: ", numerosEmbaralhados);
